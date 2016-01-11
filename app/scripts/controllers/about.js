d43790ee972ba0e04f522e66fe17ca1267b2719b
Kaspar Manz <kaspar@xeophin.net>
2016-01-11 10:58:56 +0100

    Installed yeoman angular-generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name swissnamesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the swissnamesApp
+ */
+angular.module('swissnamesApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name swissnamesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the swissnamesApp
+ */
+angular.module('swissnamesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
